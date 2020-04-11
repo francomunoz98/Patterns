088490c7778b6d3a3d0a0eb805a8782094053ec2
francomunoz98 <63399725+francomunoz98@users.noreply.github.com>
2020-04-11 23:32:22 +0200

    Created rows and columns variables

--- src/ForLoopPatterns/Patterns.java
+++ src/ForLoopPatterns/Patterns.java
@@ -1,10 +1,15 @@
 package ForLoopPatterns;
+import java.util.Scanner;
 
 public class Patterns {
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int rows, columns;
+		rows = scan.nextInt();
 		
-			
+		columns = scan.nextInt();
+		
 			for(int i=1; i<=4; i++)
 			{	
 				for(int j=1; j<=4; j++)
@@ -14,7 +19,7 @@ public class Patterns {
 				System.out.println();
 			}
 		
-
+		scan.close();
 	}
 
 }
