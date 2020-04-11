39db72bf2893388bbabf66f2f7d286053f943fe9
francomunoz98 <63399725+francomunoz98@users.noreply.github.com>
2020-04-11 23:26:55 +0200

    changed from shape from 3x5 to 4x4

--- src/ForLoopPatterns/Patterns.java
+++ src/ForLoopPatterns/Patterns.java
@@ -5,9 +5,9 @@ public class Patterns {
 	public static void main(String[] args) {
 		
 			
-			for(int i=1; i<=3; i++)
+			for(int i=1; i<=4; i++)
 			{	
-				for(int j=1; j<=5; j++)
+				for(int j=1; j<=4; j++)
 				{
 					System.out.print("*");
 				}
